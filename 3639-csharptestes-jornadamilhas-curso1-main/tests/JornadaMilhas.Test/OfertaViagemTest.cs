b958daad9c39e8ad1007b2615e0320f3811f6b37
Gustavo Vieira <gustavieiradev@gmail.com>
2025-04-21 03:51:23 -0300

    feat: Aprendemos o padrão AAA e aplicamos nos nossos testes unitarios.

--- 3639-csharptestes-jornadamilhas-curso1-main/tests/JornadaMilhas.Test/OfertaViagemTest.cs
+++ 3639-csharptestes-jornadamilhas-curso1-main/tests/JornadaMilhas.Test/OfertaViagemTest.cs
@@ -7,14 +7,32 @@ namespace JornadaMilhas.Test
         [Fact]
         public void TestandoOfertaValida()
         {
+            // Padrão Triplo A - AAA (Arrange, Act e Assert)
+
+            //cenário - arrange
             Rota rota = new Rota("OrigemTeste", "DestinoTeste");
             Periodo periodo = new Periodo(new DateTime(2024, 1, 1), new DateTime(2024, 3, 3));
             double preco = 80.0;
             var validacao = true;
 
+            //ação - act
             OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
 
+            //validação - assert
             Assert.Equal(validacao, oferta.EhValido);
+        }
+
+        [Fact]
+        public void TestandoOfertaComRotaNula()
+        {
+            Rota rota = null;
+            Periodo periodo = new Periodo(new DateTime(2024, 1, 1), new DateTime(2024, 3, 3));
+            double preco = 80.0;
+
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+            Assert.Contains("A oferta de viagem não possui rota ou período válidos.", oferta.Erros.Sumario);
+            Assert.False(oferta.EhValido);
         }
     }
 }
