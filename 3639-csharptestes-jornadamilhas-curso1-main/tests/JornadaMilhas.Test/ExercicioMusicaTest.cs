ac30f743346ff496a324ff8c4503867436cba346
Gustavo Vieira <gustavieiradev@gmail.com>
2025-04-21 04:13:08 -0300

    feat: Aplicado os Testes unitários nas atividades de Música, e criado os cenários possíveis.

---
+++ 3639-csharptestes-jornadamilhas-curso1-main/tests/JornadaMilhas.Test/ExercicioMusicaTest.cs
@@ -0,0 +1,59 @@
+﻿using JornadaMilhas.Exercicio;
+using JornadaMilhasV1.Modelos;
+
+namespace JornadaMilhas.Test
+{
+    public class ExercicioMusicaTest
+    {
+        [Fact]
+        public void TesteNomeInicializadoCorretamente()
+        {
+            // Padrão Triplo A - AAA (Arrange, Act e Assert)
+
+            //cenário - arrange
+            string nome = "Tell me Why?";
+
+            //ação - act
+            Musica musica = new Musica(nome);
+
+            //validação - assert
+            Assert.Equal(nome, musica.Nome);
+        }
+
+        [Fact]
+        public void TesteIdentificadorInicializadoCorretamente()
+        {
+            // Padrão Triplo A - AAA (Arrange, Act e Assert)
+
+            //cenário - arrange
+            int id = 1;
+            string nome = "Tell me Why?";
+
+            //ação - act
+            Musica musica = new Musica(nome) { Id = id };
+
+            //validação - assert
+            Assert.Equal(id, musica.Id);
+        }
+
+        [Fact]
+        public void TesteToString()
+        {
+            // Padrão Triplo A - AAA (Arrange, Act e Assert)
+
+            //cenário - arrange
+            int id = 1;
+            string nome = "Tell me Why?";
+            Musica musica = new Musica(nome);
+            musica.Id = id;
+            string toStringEsperado = @$"Id: {id} Nome: {nome}";
+
+            //ação - act
+            string resultado = musica.ToString();
+
+
+            //validação - assert
+            Assert.Equal(toStringEsperado, musica.ToString());
+        }
+    }
+}
