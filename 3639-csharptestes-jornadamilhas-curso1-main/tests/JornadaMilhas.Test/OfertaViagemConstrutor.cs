d93dbda7f10e3064f01944e75466b07033fca1b2
Gustavo Vieira <gustavieiradev@gmail.com>
2025-04-21 05:05:10 -0300

    feat: Aplicado o Theory para validar vários cenários possíveis de retorno dentro de um único Test.

--- 3639-csharptestes-jornadamilhas-curso1-main/tests/JornadaMilhas.Test/OfertaViagemConstrutor.cs
+++ 3639-csharptestes-jornadamilhas-curso1-main/tests/JornadaMilhas.Test/OfertaViagemConstrutor.cs
@@ -4,16 +4,20 @@ namespace JornadaMilhas.Test
 {
     public class OfertaViagemConstrutor
     {
-        [Fact]
-        public void RetornaOfertaValidaQuandoDadosValidos()
+        [Theory]
+        [InlineData("", null, "2020-01-01", "2020-01-02", 0, false)]
+        [InlineData("OrigemTeste", "DestinoTeste", "2024-01-01", "2024-03-03", 80.0, true)]
+        [InlineData(null, "São Paulo", "2024-01-01", "2024-01-02", -1, false)]
+        [InlineData("Vitória", "São Paulo", "2024-01-01", "2024-01-01", 0, false)]
+        [InlineData("Rio de Janeiro", "São Paulo", "2024-01-01", "2024-01-02", -500, false)]
+        public void RetornaEhValidoDeAcordoComDadosDeEntrada(string origem, string destino,
+            string dataIda, string dataVolta, double preco, bool validacao)
         {
             // Padrão Triplo A - AAA (Arrange, Act e Assert)
 
             //cenário - arrange
-            Rota rota = new Rota("OrigemTeste", "DestinoTeste");
-            Periodo periodo = new Periodo(new DateTime(2024, 1, 1), new DateTime(2024, 3, 3));
-            double preco = 80.0;
-            var validacao = true;
+            Rota rota = new Rota(origem, destino);
+            Periodo periodo = new Periodo(DateTime.Parse(dataIda), DateTime.Parse(dataVolta));
 
             //ação - act
             OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
