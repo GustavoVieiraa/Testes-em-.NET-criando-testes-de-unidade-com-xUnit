51588a011c004b1b2e7d88be44d21e44f83b6a08
Gustavo Vieira <gustavieiradev@gmail.com>
2025-04-22 02:16:22 -0300

    feat: Implementando os filtros em nosso teste.

--- 3639-csharptestes-jornadamilhas-curso1-main/tests/JornadaMilhas.Test/GerenciadorDeOfertasRecuperaMaiorDesconto.cs
+++ 3639-csharptestes-jornadamilhas-curso1-main/tests/JornadaMilhas.Test/GerenciadorDeOfertasRecuperaMaiorDesconto.cs
@@ -51,8 +51,15 @@ namespace JornadaMilhas.Test
                 Ativa = true
             };
 
+            var ofertaInativa = new OfertaViagem(rota, fakerPeriodo.Generate(), 70)
+            {
+                Desconto = 40,
+                Ativa = false
+            };
+
             var lista = fakerOferta.Generate(200);
             lista.Add(ofertaEscolhida);
+            lista.Add(ofertaInativa);
             var gerenciador = new GerenciadorDeOfertas(lista);
             Func<OfertaViagem, bool> filtro = o => o.Rota.Destino.Equals("São Paulo");
             double precoEsperado = 40.00;
